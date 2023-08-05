f6fa2379d382c15f85792418119473a905a90abb
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-05 14:51:25 -0500

    Make progress.

--- coin_flips_streak.py
+++ coin_flips_streak.py
@@ -14,34 +14,37 @@ logging.basicConfig(
 logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
-def streak_counter(number):
+def streak_counter(number: int) -> str:
     number_of_streaks = 0
 
     for _ in range(number):
-        results = []
-        streak = []
-        heads = 0
-        tails = 0
+        results, streak, heads, tails = [], [], 0, 0
+
         for _ in range(FLIPS_PER_TRIAL):
             if random.randint(0, 1) == 0:
                 results.append("H")
             else:
                 results.append("T")
+
         while results[heads] == "H":
             streak.append("H")
             heads += 1
+
             if len(streak) >= MIN_STREAK:
                 number_of_streaks += 1
+
         while results[tails] == "T":
             streak.append("T")
             tails += 1
+
             if len(streak) >= MIN_STREAK:
                 number_of_streaks += 1
-    print(f"Chance of streak: {number_of_streaks / 100}%")
+
+    return f"Chance of streak: {number_of_streaks / 100}%"
 
 
-def main():
-    streak_counter(NUMBER_OF_TRIALS)
+def main() -> None:
+    print(streak_counter(NUMBER_OF_TRIALS))
 
 
 if __name__ == "__main__":
