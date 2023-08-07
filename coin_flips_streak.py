7c9b94db0be8d16237afadab5119cb42df8a03e1
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-07 14:01:31 -0500

    Move refactored code into coin_flips_streak.py.

--- coin_flips_streak.py
+++ coin_flips_streak.py
@@ -2,9 +2,10 @@
 # coin_flips_streak.py — An exercise in understanding lists.
 # For more information, see README.md
 
+from itertools import groupby
 import random
 import logging
-from constants import NUMBER_OF_TRIALS, FLIPS_PER_TRIAL, MIN_STREAK
+from constants import NUMBER_OF_TRIALS, NUMBER_OF_FLIPS, STREAK_MINIMUM
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -13,44 +14,53 @@ logging.basicConfig(
 )
 # logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
-"""This isn't working as intended. Fix before making public."""
+
+def main():
+    total = count_streaks()
+    percentage = total / 100
+    print(f"Chance of streak of {STREAK_MINIMUM}: {percentage}%")
 
 
-def streak_counter(number: int) -> str:
-    number_of_streaks = 0
+def flip_coin(number: int) -> list:
+    results = [random.choice(["H", "T"]) for _ in range(number)]
+    # Non-list-comprehension version for comparison
+    # results = []
+    # for _ in range(number):
+    #     results.append(random.choice(["H", "T"]))
+    logging.debug(results)
 
-    for _ in range(number):
-        results, streak, heads, tails = [], [], 0, 0
-
-        for _ in range(FLIPS_PER_TRIAL):
-            if random.randint(0, 1) == 0:
-                results.append("H")
-            else:
-                results.append("T")
-        logging.debug(results)
-
-        while results[heads] == "H":
-            streak.append("H")
-            heads += 1
-
-            if len(streak) == MIN_STREAK:
-                logging.debug(streak)
-                number_of_streaks += 1
-
-        while results[tails] == "T":
-            streak.append("T")
-            tails += 1
-
-            if len(streak) == MIN_STREAK:
-                logging.debug(streak)
-                number_of_streaks += 1
-
-    percentage = number_of_streaks / FLIPS_PER_TRIAL
-    return f"Chance of streak: {percentage}%"
+    return results
 
 
-def main() -> None:
-    print(streak_counter(NUMBER_OF_TRIALS))
+def count_duplicates(results: list) -> int:
+    streak_counter = 0
+    streak = [len(list(group)) for _, group in groupby(results)]
+    # Non-list-comprehension version for comparison
+    # streak = []
+    # for _, group in groupby(results):
+    #     streak.append(len(list(group)))
+
+    for i in streak:
+        if i >= STREAK_MINIMUM:
+            streak_counter += 1
+
+    logging.debug(streak)
+    logging.debug(streak_counter)
+
+    return streak_counter
+
+
+def count_streaks():
+    streak_total = 0
+
+    for _ in range(NUMBER_OF_TRIALS):
+        results = flip_coin(NUMBER_OF_FLIPS)
+        if count_duplicates(results) > 0:
+            streak_total += 1
+
+    logging.debug(streak_total)
+
+    return streak_total
 
 
 if __name__ == "__main__":
