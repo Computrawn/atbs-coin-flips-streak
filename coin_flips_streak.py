5179d356b53fe30f0b67ac2336726f5f0d7f84cf
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-08 14:01:41 -0500

    Refine find_streak and add function documentation.

--- coin_flips_streak.py
+++ coin_flips_streak.py
@@ -16,6 +16,7 @@ logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
 def main() -> None:
+    """Run calculations on results to determine likelihood of streak and print results."""
     total = count_streaks()
     percentage = total / (NUMBER_OF_TRIALS / NUMBER_OF_FLIPS)
     print(
@@ -24,35 +25,35 @@ def main() -> None:
 
 
 def flip_coin(number: int) -> list[str]:
+    """Create and return a list of n H's and T's."""
     results = [choice(["H", "T"]) for _ in range(number)]
-    # Non-list-comprehension version for comparison
-    # results = []
-    # for _ in range(number):
-    #     results.append(random.choice(["H", "T"]))
-    logging.debug(results)
 
+    logging.debug(results)
     return results
 
 
 def find_streak(results: list[str]) -> int:
+    """Use groupby to convert consecutive H and T values to integers,
+    cast those values to a list, reverse sort the new list and increment a counter
+    if the first item is greater than the minimum streak value. Return counter."""
     streak_counter = 0
     streak = [len(list(group)) for _, group in groupby(results)]
     # Non-list-comprehension version for comparison
     # streak = []
     # for _, group in groupby(results):
     #     streak.append(len(list(group)))
-
-    for i in streak:
-        if i >= STREAK_MINIMUM:
-            streak_counter += 1
-
     logging.debug(streak)
-    logging.debug(streak_counter)
+    streak.sort(reverse=True)
 
+    if streak[0] >= STREAK_MINIMUM:
+        streak_counter += 1
+
+    logging.debug(streak_counter)
     return streak_counter
 
 
 def count_streaks() -> int:
+    """Get result from n number of trials, increment streak total counter and return it."""
     streak_total = 0
 
     for _ in range(NUMBER_OF_TRIALS):
@@ -61,7 +62,6 @@ def count_streaks() -> int:
             streak_total += 1
 
     logging.debug(streak_total)
-
     return streak_total
 
 
